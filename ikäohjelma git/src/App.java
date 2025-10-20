5e5e2c4995705813c48e7c052653abda625f6db8
kaisakorteniemi <kaisa.korteniemi@gmail.com>
2025-10-20 22:54:20 +0300

    Update App.java
    Poistettu tehtävänanto koodin välistä

--- ikäohjelma git/src/App.java
+++ ikäohjelma git/src/App.java
@@ -2,14 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         int ika = 20;
 
-//Tehtävä 1: Laajenna ohjelmaa siten, että ohjelma osaa kertoa 16-17-vuotiaille, että he voivat ajaa kevaria.
-//Tehtävä 2: Laajenna ohjelmaa niin, että se kertoo 18-vuotiaalle, että hän on juuri tullut täysi-ikäiseksi ja saa ajaa autoa.
-//Tehtävä 3: Laajenna tehtävää niin, että aikuisille ilmoitetaan tasavuosikymmenistä onnentoivotus.
-//Tehtävä 4: jos ikä on 100, tulosta 3-rivinen onnentoivotus.
-//Tehtävä 5: Tarkenna eläkeasioita siten, että yli 58-vuotiaille kerro, että he voivat mennä varhaiseläkkeelle.
-//Tehtävä 6: Toivota 65-vuotiaille hyviä eläkepäiviä.
-//Tehtävä 7: 40–50 -vuotiaille toivota parasta keski-ikää.
-
     if (ika > 0 && ika < 18) {
         System.out.println("Olet alaikäinen");
         if (ika >= 15) {
@@ -46,4 +38,5 @@ public class App {
     }
 
 }
+
 
